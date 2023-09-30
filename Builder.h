febef2dab10a1d00b348d8dfe73e79591256bad0
Oles <oles1stepanov@gmail.com>
2023-09-30 10:30:51 +0300

    Fix builder

--- Builder.h
+++ Builder.h
@@ -5,10 +5,11 @@
 class Product
 {
 public:
-    Product();
-    void SetAtribute1(int atribute) {_atribute_1 = atribute;}
+    Product() {std::cout << "-- product constructor --" << std::endl;};
+    ~Product() {std::cout << "-- product destructor --" << std::endl;}
+    void SetAtribute1(int atribute)         {_atribute_1 = atribute;}
     void SetAtribute2(std::string atribute) {_atribute_2 = atribute;}
-    void SetAtribute3(bool atribute) {_atribute_3 = atribute;}
+    void SetAtribute3(bool atribute)        {_atribute_3 = atribute;}
 
     void Show()
     {   std::cout << "Product constructed : " << std::endl;
@@ -24,20 +25,22 @@ private:
 class IBuilder
 {
 public:
-    virtual void BuildAtribute_1() = 0;
-    virtual void BuildAtribute_2() = 0;
-    virtual void BuildAtribute_3() = 0;
-    virtual Product getProduct() = 0;
+    virtual void    BuildAtribute_1() = 0;
+    virtual void    BuildAtribute_2() = 0;
+    virtual void    BuildAtribute_3() = 0;
+    virtual Product getProduct()      = 0;
 
 };
 
 class ProductBuilder : public IBuilder
 {
 public:
+    ProductBuilder()  {_product = new Product(); std::cout << "-- product builder constructor --" << std::endl;}
+    ~ProductBuilder() {std::cout << "-- product builder destructor --" << std::endl;}
     void    BuildAtribute_1()  override     {_product->SetAtribute1(10);}
     void    BuildAtribute_2()  override     {_product->SetAtribute2("AAA");}
     void    BuildAtribute_3()  override     {_product->SetAtribute3(true);}
-    Product getProduct()       override     {return *_product;}
+    Product getProduct()       override     {std::cout << "Getting product" << std::endl; return *_product;}
 private:
     Product *_product;
 };
@@ -45,9 +48,11 @@ private:
 class Director
 {
 public:
-    Director(IBuilder* builder) : _builder(builder){}
+    Director(IBuilder* builder) : _builder(builder){std::cout << "-- director constructor --" << std::endl;}
+    ~Director() {std::cout << "-- director destructor --" << std::endl;}
     void ConstructProduct()
     {
+        std::cout << "Construct product" << std::endl;
         _builder->BuildAtribute_1();
         _builder->BuildAtribute_2();
         _builder->BuildAtribute_3();
@@ -55,10 +60,4 @@ public:
 private:
     IBuilder* _builder;
 };
-
-
-
-
-
-
 #endif // BUILDER_H
--- main.cpp
+++ main.cpp
@@ -1,24 +1,25 @@
 #include <QCoreApplication>
 #include <iostream>
-#include "Builder.h"
+
+#include "builder.h"
 
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
 
+    // CREATE A BUILDER
     IBuilder *builder = new ProductBuilder();
+
+    //CREATE A DIRECTOR WHO MANEGEs BUILDER
     Director director(builder);
 
     director.ConstructProduct();
 
+    //CONSTRUCT PRODUCT WITH BUILDER
     std::cout << "Build Product" << std::endl;
-
     Product prod = builder->getProduct();
     prod.Show();
-
-
-
 
     return a.exec();
 }
